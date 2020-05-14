77cb9de586582d6dd63b8a289b6a7191dfcf3f27
Yufeng Su <yufengs2@illinois.edu>
2020-05-14 16:23:15 -0500

    kl recalculated.

--- src/main.py
+++ src/main.py
@@ -124,8 +124,8 @@ def main(dir, output_dir,top = 100):
             if i + motiflength > len(sequence):
                 continue
             probs.append(get_prob(motif, sequence[i:i + motiflength]))
-        # best_prob_id = probs.index(max(probs))
-        best_prob_id = random.randint(0, len(sequence) - motiflength - 1)
+        best_prob_id = probs.index(max(probs))
+        # best_prob_id = random.randint(0, len(sequence) - motiflength - 1)
         site_results.append(best_prob_id)
         site_sequences.append(sequence[best_prob_id: best_prob_id+motiflength])
         best_motifs.append(sequence[best_prob_id: best_prob_id+motiflength])
@@ -140,7 +140,7 @@ def main(dir, output_dir,top = 100):
     correct_site_predicted = sum([1 if check_overlap(i,j) else 0 for i, j in zip(site_sequences, true_sites)])
     motifs = np.array(motifs).reshape((-1))
     best_motifs = np.array(best_motifs).reshape((-1))
-    kl_dis = kl_divergence(motifs+0.00001, best_motifs+0.00001)
+    kl_dis = kl_divergence(best_motifs+0.00001, motifs+0.00001)
     return correct_site_predicted,correct_pos_predicted, acc, kl_dis
 
 
@@ -178,7 +178,7 @@ correct_poss_predicted = []
 correct_sites_predicted = []
 for parameter in parameters:
     for i in range(10):
-        correct_site_predicted,correct_pos_predicted,acc, kl_dis = main("../" + parameter + "/trial" + str(i), "../output(random)/" + parameter + "_trial" + str(i),50)
+        correct_site_predicted,correct_pos_predicted,acc, kl_dis = main("../" + parameter + "/trial" + str(i), "../output(top=100)/" + parameter + "_trial" + str(i),100)
         accs.append(acc)
         correct_poss_predicted.append(correct_pos_predicted)
         correct_sites_predicted.append(correct_site_predicted)
@@ -198,6 +198,6 @@ data['correct_pos_predicted'] = correct_poss_predicted
 data['correct_sites_predicted'] = correct_sites_predicted
 
 df = pd.DataFrame(data)
-df.to_csv("../output(random)/summary.tsv", sep='\t', index=False, columns=['parameters', 'trial_id', 'accuracy','correct_pos_predicted', 'correct_sites_predicted','kl_dis'])
+df.to_csv("../output(top=100)/summary.tsv", sep='\t', index=False, columns=['parameters', 'trial_id', 'accuracy','correct_pos_predicted', 'correct_sites_predicted','kl_dis'])
 
 
